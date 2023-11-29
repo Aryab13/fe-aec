ec55c0549a09312d417318412172e9d018f12a41
Aryab13 <223210059@student.unaki.ac.id>
2023-11-29 20:12:35 +0700

    add forgotpass

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/login"
 import Register from "./pages/register"
 import LiveSession from './pages/live-session'
 import DetailLs from './pages/detail-ls'
+import ForgotPass from './pages/forgot-pass'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route element={<Index/>} path='/'/>
         <Route element={<Login/>} path='/login'/>
         <Route element={<Register/>} path='/register'/>
+        <Route element={<ForgotPass/>} path='/forgotpass'/>
         <Route element={<LiveSession/>} path='/ls'/>
         <Route element={<DetailLs/>} path='/detaills/:id'/>
       </Routes>
--- src/pages/forgot-pass.jsx
+++ src/pages/forgot-pass.jsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 
-function Login() {
+function ForgotPass() {
 
     const navigate = useNavigate();
-    
+
     const BASE_API = "https://652ff3566c756603295dfc8e.mockapi.io";
 
     const fetcher = async ({ endpoint = "", config = {} }) => {
@@ -37,21 +37,29 @@ function Login() {
         });
       
         const isUserExist = data?.find(
-          (item) => item.email === inputEmail && item.password === inputPassword
+          (item) => item.email === inputEmail
         );
       
-        if (!isUserExist) return alert("Wrong Email or Password");
+        if (!isUserExist) return alert("Email not Registered");
       
-        localStorage.setItem(
-          "usr",
-          JSON.stringify({
-            id: isUserExist.id,
-            name: isUserExist.name,
-          })
-        );
-        navigate("/");
+        fetcher({
+            endpoint: "user",
+            config: {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                email: inputEmail,
+                password: inputPassword,
+              }),
+            },
+          });
+        
+        alert("Reset Password Successful");
+        navigate("/login");
       };
-
+    
     const togglePassword = () => {
         var passwordInput = document.getElementById('password');
         if (passwordInput.type === 'password') {
@@ -61,16 +69,16 @@ function Login() {
         }
     }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit} class="max-w-sm mx-auto mt-32">
-                <h1 className='text-center text-3xl font-bold pb-6'>Sign In</h1>
+  return (
+    <>
+        <form onSubmit={handleSubmit} class="max-w-sm mx-auto mt-36">
+                <h1 className='text-center text-3xl font-bold pb-6'>Reset Password</h1>
                 <div class="mb-5">
                     <label for="email" class="block mb-2 text-base font-medium">Your email</label>
                     <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-100 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email..." required/>
                 </div>
                 <div class="mb-5">
-                    <label for="password" class="block mb-2 text-base font-medium">Your password</label>
+                    <label for="password" class="block mb-2 text-base font-medium">Reset password</label>
                     <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-100 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password..." required/>
                 </div>
                 <div class="flex items-start mb-5">
@@ -79,12 +87,10 @@ function Login() {
                     </div>
                     <label for="show" class="ms-2 text-sm font-medium">Show Password</label>
                 </div>
-                <p onClick={() => {navigate('/forgotpass');window.scrollTo(0, 0);}} className="text-sm pb-5 text-green-600 hover:underline cursor-pointer">Forgot Password?</p>
                 <button type="submit" class="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
-                <p className="text-right">Don't have an account? <a onClick={() => {navigate('/register');window.scrollTo(0, 0);}} className="text-green-600 hover:underline cursor-pointer">Signup</a></p>
             </form>
-        </>
-    )
+    </>
+  )
 }
 
-export default Login
+export default ForgotPass
